401bcce5c8251063719a8913a041d2ce718c23c9
FrankWhoee <huifrank@live.com>
2023-05-28 19:06:43 -0700

    Add aging

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ options.disable_hardware_pulsing = True
 
 matrix = RGBMatrix(options = options)
 
-life = [[0] * options.cols for i in range(options.rows)]
+life = [[[0,0]] * options.cols for i in range(options.rows)]
+
+MAX_AGE = 20
 
 def applyToEveryCell(fn, grid):
     changed_values = []
@@ -30,7 +32,10 @@ def applyToEveryCell(fn, grid):
             if val is not None:
                 changed_values.append((fn(grid,r,c), r, c))
     for point in changed_values:
-        grid[point[1]][point[2]] = point[0]
+        if point[0] == 1:
+            grid[point[1]][point[2]] = [1,MAX_AGE]
+        else:
+           grid[point[1]][point[2]] = [0,0] 
     return changed_values
 
 def randomizeCell(grid, r,c):
@@ -39,7 +44,7 @@ def randomizeCell(grid, r,c):
 def addRandomCell(grid, r,c):
     liveNeighbours = getLiveNeighbours(grid,r,c)
     if liveNeighbours > 0:
-        return 1 if random.random() < 0.05 else grid[r][c]
+        return 1 if random.random() < 0.05 else grid[r][c][0]
 
 def getLiveNeighbours(grid, r,c):
     alive = 0
@@ -52,33 +57,45 @@ def getLiveNeighbours(grid, r,c):
 
         if (r1 < 0 or r1 >= len(grid) or c1 < 0 or c1 >= len(grid[0])):
             continue
-        if grid[r1][c1] == 1:
+        if grid[r1][c1][0] == 1:
             alive += 1
 
     return alive
 
 def lifeCell(grid, r,c):
     liveNeighbours = getLiveNeighbours(grid,r,c)
-    if grid[r][c] == 1:
+    if grid[r][c][0] == 1:
         if liveNeighbours < 2 or liveNeighbours > 3:
             return 0
-    elif grid[r][c] == 0 and liveNeighbours == 3:
+    elif grid[r][c][0] == 0 and liveNeighbours == 3:
             return 1
     return None
+
+def ageCell(grid, r, c):
+    if grid[r][c][0] == 1:
+        grid[r][c][1] -= 1
+    if grid[r][c][1] <= 0:
+        return 0
     
 
-# applyToEveryCell(randomizeCell, life)
-life[32][30] = 1
-life[32][31] = 1
-life[32][32] = 1
+def getPopulation(grid):
+    pop = 0
+    for r in grid:
+        for c in r:
+            if c[0] == 1:
+                pop += 1
+    return pop
+
+applyToEveryCell(randomizeCell, life)
 
 def constructCanvas():
     global life
     canvas = matrix.CreateFrameCanvas()
 
     def helper(grid, r, c):
-        if grid[r][c] == 1:
-            canvas.SetPixel(r,c, 255,125,0)
+        if grid[r][c][0] == 1:
+            age_multiplier = grid[r][c][1]/MAX_AGE
+            canvas.SetPixel(r,c, int(255 * age_multiplier),int(125 * age_multiplier),0)
     
     applyToEveryCell(helper, life)
     return canvas
@@ -88,7 +105,8 @@ def canvasClear():
     canvas = matrix.CreateFrameCanvas()
 
     def helper(grid, r,c):
-        grid[r][c] = 0
+        grid[r][c][0] = 0
+        grid[r][c][1] = 0
         canvas.SetPixel(r,c,0,0,0)
     applyToEveryCell(helper,life)
 
@@ -102,8 +120,10 @@ try:
     now = datetime.now().time()
     print(now.hour)
     boardOn = now.hour < 23 and now.hour >= 9
+    idleCount = 7
     while(True):
         matrix.SwapOnVSync(constructCanvas())
+        applyToEveryCell(ageCell, life)
         now = datetime.now().time()
         if now.hour == 22 and now.minute >= 30:
             boardOn = False
@@ -117,9 +137,13 @@ try:
         if len(curr) == 0:
             applyToEveryCell(randomizeCell, life)
         elif prev == curr or curr == prevPrev:
+            idleCount -= 1
+        if idleCount <= 0:
             applyToEveryCell(addRandomCell, life)
+            idleCount = 7
         prevPrev = prev
         prev = curr
-        time.sleep(0.05)
+        pop = getPopulation(life)
+        time.sleep(25/(pop + 50))
 except KeyboardInterrupt:
     sys.exit(0)
